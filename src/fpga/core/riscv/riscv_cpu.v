28388f73f8dedc963b5e23e716a3ac0d717e4d45
Guangyuan Niu <niuguangyuan@gmail.com>
2024-10-28 20:36:48 -0500

    add ios for memory

--- src/fpga/core/riscv/riscv_cpu.v
+++ src/fpga/core/riscv/riscv_cpu.v
@@ -1,6 +1,13 @@
 module riscv_cpu(
     input clk,
-    input reset_n
+    input reset_n,
+    output [31:0] instruction_memory_address,
+    input [31:0] instruction_memory_instruction,
+    output [31:0] data_memory_address,
+    output [31:0] data_memory_write_data,
+    output data_memory_mem_write,
+    output data_memory_mem_read,
+    input [31:0] data_memory_read_data
 );
     reg [31:0] pc;
     wire [31:0] pc_next;
