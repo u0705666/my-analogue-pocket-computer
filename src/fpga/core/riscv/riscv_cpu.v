7c9d60db00e7bf4a2683e062f19e54afdc299972
Guangyuan Niu <niuguangyuan@gmail.com>
2024-10-30 21:09:07 -0500

    add alu control logic

--- src/fpga/core/riscv/riscv_cpu.v
+++ src/fpga/core/riscv/riscv_cpu.v
@@ -1,3 +1,6 @@
+`include "riscv_configs.v"
+`default_nettype none
+
 module riscv_cpu(
     input clk,
     input reset_n,
@@ -34,6 +37,51 @@ module riscv_cpu(
         .rd1(rd1),
         .rd2(rd2)
     );
+
+    // Alu32 instance
+    wire [31:0] alu_result;
+    wire alu_zero;
+
+    //bind these to the register file later
+    wire [31:0] alu_a;
+    wire [31:0] alu_b;
+
+    Alu32 alu (
+        .control(alu_control),
+        .a(alu_a),
+        .b(alu_b),
+        .out(alu_result),
+        .zero(alu_zero)
+    );
+
+    // ALU control unit
+    wire [1:0] ALUOp;
+    wire [6:0] funct7;
+    wire [2:0] funct3;
+    reg [3:0] alu_control;
+
+    // Extract funct7 and funct3 from the instruction
+    assign funct7 = instruction_memory_instruction[31:25];
+    assign funct3 = instruction_memory_instruction[14:12];
+
+    // ALU control logic
+    always @(*) begin
+        case (ALUOp)
+            2'b00: alu_control = `ALU_ADD; // add for loads and stores
+            2'b01: alu_control = `ALU_SUB; // subtract for beq
+            2'b10: begin
+                case ({funct7, funct3})
+                    10'b0000000_000: alu_control = `ALU_ADD; // add
+                    10'b0100000_000: alu_control = `ALU_SUB; // subtract
+                    10'b0000000_111: alu_control = `ALU_AND; // and
+                    10'b0000000_110: alu_control = `ALU_OR; // or
+                    default: alu_control = `ALU_AND; // default to and
+                endcase
+            end
+            default: alu_control = `ALU_AND; // default to and
+        endcase
+    end
+    
 
 
 
